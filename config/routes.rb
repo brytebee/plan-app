d4fc077ba7d98c5c1e4bda6d86d13d6e92074958
brytebee <brytebee@gmail.com>
2022-05-22 22:40:05 +0100

    :fire: Clean up
    Clean up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'splash_screen/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'splash_screen/home'
 end
