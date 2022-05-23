6b7a59bb291f1a92792f687c757b9a13a0a23e61
brytebee <brytebee@gmail.com>
2022-05-23 20:25:38 +0100

    :white_check_mark: Update categories_controller
    Change param user_id to author_id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -66,6 +66,6 @@ class CategoriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def category_params
-      params.require(:category).permit(:name, :icon, :user_id)
+      params.require(:category).permit(:name, :icon, :author_id)
     end
 end
