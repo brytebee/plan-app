49bf29a008566c8ef7e6beab0c509588b65f85d1
brytebee <brytebee@gmail.com>
2022-05-24 00:56:06 +0100

    :white_check_mark: Update category model
    Add associations and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: "User", foreign_key: "author_id"
+  has_many :transactions
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
