c4a077111765079874f3e6dd4d40b0700c993137
brytebee <brytebee@gmail.com>
2022-05-23 18:51:17 +0100

    :white_check_mark: Update schema.rb
    Migrated table

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_154914) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_160129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
+    t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
   create_table "transactions", force: :cascade do |t|
     t.string "name"
     t.money "amount", scale: 2
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_transactions_on_author_id"
     t.index ["category_id"], name: "index_transactions_on_category_id"
-    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,7 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_154914) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  add_foreign_key "categories", "users"
+  add_foreign_key "categories", "users", column: "author_id"
   add_foreign_key "transactions", "categories"
-  add_foreign_key "transactions", "users"
+  add_foreign_key "transactions", "users", column: "author_id"
 end
