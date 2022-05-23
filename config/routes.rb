9d2bf4839588ff3f67c2ca9e0aec9e076df5d4c4
brytebee <brytebee@gmail.com>
2022-05-23 18:56:42 +0100

    :white_check_mark: Update routes.rb
    Nest transactions into categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :transactions
+  resources :categories do
+    resources :transactions
+  end
   devise_for :users
   root 'splash_screen#home'
 end
