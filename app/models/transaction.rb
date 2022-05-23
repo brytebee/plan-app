b018ea2ecc5b0bd3dc270cba74ab24d3977a6eb0
brytebee <brytebee@gmail.com>
2022-05-24 00:52:16 +0100

    :white_check_mark: Update transaction model
    Update association and add validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: "User", foreign_key: "author_id"
   belongs_to :category
+
+  validates :name, presence: true
+  validate :amount, presence: true, numericality: { money: true, greater_than_or_equal_to: 0 }
 end
