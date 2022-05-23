524d0ebe0e18c54b83c9f11bedcca344137307a8
brytebee <brytebee@gmail.com>
2022-05-23 19:35:54 +0100

    :white_check_mark: Update routes.rb
    Conditionally render a separare root route for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
     resources :transactions
   end
   devise_for :users
+  devise_scope :user do
+    authenticated do
+      root :to => 'category#index' as: authenticated_root
+      get '/users/sign_out' => 'devise/session#destroy'
+    end
+  end
   root 'splash_screen#home'
 end
