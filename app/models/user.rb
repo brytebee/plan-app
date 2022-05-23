7ddc51b6c1aae0ce130306132c93d231f96073ce
brytebee <brytebee@gmail.com>
2022-05-24 00:43:45 +0100

    :white_check_mark: Update user model
    Added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :categories, foreign_key: 'author_id', dependent: :destroy
+  has_many :transactions, foreign_key: 'author_id', dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
