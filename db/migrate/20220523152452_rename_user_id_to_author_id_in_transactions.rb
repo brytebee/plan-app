f66a3678b301348209357ecc2328e49027da449a
brytebee <brytebee@gmail.com>
2022-05-23 16:29:08 +0100

    :white_check_mark: Migrate column name change
    From user_id to author_id

--- db/migrate/20220523152452_rename_user_id_to_author_id_in_transactions.rb
+++ db/migrate/20220523152452_rename_user_id_to_author_id_in_transactions.rb
@@ -1,5 +1,5 @@
 class RenameUserIdToAuthorIdInTransactions < ActiveRecord::Migration[7.0]
   def change
-    rename_column :table_name, :old_column, :new_column
+    rename_column :transactions, :user_id, :author_id
   end
 end
