a011cba43643355b6bad43dab6fcf5eb2e3c3b81
brytebee <brytebee@gmail.com>
2022-05-24 11:39:29 +0100

    :white_check_mark: Update ability model
    Try out a more succint syntax

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,12 +3,19 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user
+  def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
     else
-      can :read, :all
+      can :read, Category, author_id: user.id
+      can :show, Category, author_id: user.id
+      can :update, Category, author_id: user.id
+      can :destroy, Category, author_id: user.id
+      can :read, Transaction, author_id: user.id
+      can :show, Transaction, author_id: user.id
+      can :update, Transaction, author_id: user.id
+      can :destroy, Transaction, author_id: user.id
     end
   end
 end
