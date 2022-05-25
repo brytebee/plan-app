584e8ef03701eeb33ec1dd50a0c8214d5a5e4e60
brytebee <brytebee@gmail.com>
2022-05-25 16:09:16 +0100

    :white_check_mark: Display recent transactions
    Display recent transactions with date

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,11 +2,11 @@ class TransactionsController < ApplicationController
   load_and_authorize_resource
   before_action :authenticate_user!
   before_action :set_transaction, only: %i[show edit update destroy]
-  before_action :recent_transactions
 
   # GET /transactions or /transactions.json
   def index
     @transactions = Transaction.all
+    @recent_transactions = @transactions.order(created_at: :desc)
   end
 
   # GET /transactions/1 or /transactions/1.json
@@ -63,10 +63,6 @@ class TransactionsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_transaction
     @transaction = Transaction.find(params[:id])
-  end
-
-  def recent_transactions
-    @transactions.order(created_at: :desc)
   end
 
   # Only allow a list of trusted parameters through.
